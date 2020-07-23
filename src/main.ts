3a9a13973256a9b90125088fcad470bdd83633bf
caronoff <caronoff@gmail.com>
2020-07-22 22:56:12 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import Amplify, {Auth} from 'aws-amplify';
-
 import awsconfig from './aws-exports';
-
-Amplify.configure(awsconfig);
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Amplify.configure(awsconfig);
 
 
 if (environment.production) {
